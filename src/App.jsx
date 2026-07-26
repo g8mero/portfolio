4a6214a01fdec50682ce068b653db2baa0cb0d58
g8mero <gamero.reachme@gmail.com>
2026-07-26 13:22:15 -0300

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,62 @@ export default function HeroSection() {
           />
         </div>
 
-        <div className="text-center md:text-left flex flex-col justify-center min-w-[280px] sm:min-w-[400px] md:min-w-[500px]">
+        <div className="-space-y-5 text-center md:text-left flex flex-col justify-center min-w-[280px] sm:min-w-[400px] md:min-w-[500px] md:space-y-0">
           <h1 className="font-cormorant text-6xl md:text-8xl tracking-tight leading-none">
             Hey! I'm a
           </h1>
           <TypeAnimation
             sequence={[
-              'creative.', 1000,
               'developer.', 1000,
-              'traveler.', 1000,
-              'student.', 2000
+              'speaker.', 1000,
+              'leader.', 1000,
+              'creative.', 1000,
+              'student.', 1000,
             ]}
             wrapper="span"
             speed={50}
             deletionSpeed={30}
             repeat={Infinity}
-            className="font-cormorant text-6xl md:text-8xl italic block text-yellow-400"
+            className="font-cormorant text-6xl md:text-8xl italic"
           />
+          <div className = "flex flex-col items-center justify-center -space-y-14 md:flex-row md:items-startr md:justify-start md:space-y-0">
+            <div>
+              <button 
+                onClick={() => alert('Coming soon!')}
+                className="w-30 flex justify-center md:justify-start -mt-2"
+              >
+                <img 
+                  src="/codeButton.png" 
+                  alt="code" 
+                  className="w-30 h-30 object-contain flex justify-start hover:cursor-pointer"
+                />
+              </button>
+            </div>
+            <div>
+              <button 
+                onClick={() => alert('Coming soon!')}
+                className="w-30 flex justify-center md:justify-start -mt-2"
+              >
+                <img 
+                  src="/communicateButton.png" 
+                  alt="communication" 
+                  className="w-30 h-30 object-contain flex justify-start hover:cursor-pointer"
+                />
+              </button>
+            </div>
+            <div>
+              <button 
+                onClick={() => alert('Coming soon!')}
+                className="w-30 flex justify-center md:justify-start -mt-2"
+              >
+                <img 
+                  src="/conserveButton.png" 
+                  alt="conservation" 
+                  className="w-30 h-30 object-contain flex justify-start hover:cursor-pointer"
+                />
+              </button>
+            </div>
+          </div>
         </div>
 
       </div>
