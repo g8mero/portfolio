8a958fb4ec53f53ddf2f8098ef6e4cca55998679
g8mero <gamero.reachme@gmail.com>
2026-08-01 17:41:33 -0300

    added languages

--- src/Code.jsx
+++ src/Code.jsx
@@ -15,8 +15,9 @@ export default function Code(){
                 <span>code</span>
             </button>   
             <div class = " mx-auto mt-6 max-w-xl min-h-screen">
+                
                 <h1 class = "font-cormorant text-4xl">
-                    Projects
+                    Projects -------------------------------------
                 </h1> 
                 <div class = "mt-5 mb-5 flex flex-col md:flex-row items-center gap-2 md:gap-8">
                     <div>
@@ -30,13 +31,19 @@ export default function Code(){
                             GreenPages
                         </h3>
                         <p class = " font-jetbrains ">
-                            Like Grammarly, but for paper & ink. Flags wasteful print formatting in Google Docs and suggests eco-friendly fixes you accept one at a time.
+                            Like Grammarly, but for clean print jobs. Flags wasteful print formatting in Google Docs and suggests eco-friendly fixes to reduce wasting pages && ink.
                         </p>
-                        
                     </div>
                 </div>
-                <h1 class = "font-cormorant text-4xl">
-                    Hackathons
+                <h1 class = "mt-5 font-cormorant text-4xl">
+                    Languages ----------------------------------
+                </h1>
+                <p class = " mt-5 font-jetbrains text-justify [text-align-last:justify]">
+                    python java html javascript react tailwindcss 
+                </p>
+                
+                <h1 class = "mt-5 font-cormorant text-4xl">
+                    Awards --------------------------------------
                 </h1> 
                 <div class="mt-5 grid grid-cols-1 gap-6 md:grid-cols-2">
                     <a class="border-surface0 bg-base hover:border-accent focus-visible:border-accent group block overflow-hidden rounded-xl border shadow-lg transition-all duration-300 hover:shadow-xl focus:outline-none">
@@ -46,12 +53,9 @@ export default function Code(){
                             ></img>
                         </div>
                         <div class="space-y-3 p-5">
-                            <h3 class = "text-text group-hover:text-accent text-xl font-jetbrains transition-colors">
-                                USAII Global Hackathon | Finalist
+                            <h3 class = "flex flex-col text-text group-hover:text-accent text-xl font-jetbrains transition-colors">
+                                <span>USAII Global Hackathon, Finalist</span>
                             </h3>
-                            <p class="text-subtext0 line-clamp-2 text-sm">
-                                
-                            </p>
                         </div>
                     </a>
                     <a class="border-surface0 bg-base hover:border-accent focus-visible:border-accent group block overflow-hidden rounded-xl border shadow-lg transition-all duration-300 hover:shadow-xl focus:outline-none">
@@ -61,12 +65,10 @@ export default function Code(){
                             ></img>
                         </div>
                         <div class="space-y-3 p-5">
-                            <h3 class = "text-text group-hover:text-accent text-xl font-jetbrains transition-colors">
-                                Earth.io Hackathon | 2nd Place
+                            <h3 class = "flex flex-col text-text group-hover:text-accent text-xl font-jetbrains transition-colors">
+                                <span>Earth.io Hackathon,</span>
+                                <span>2nd Place</span>
                             </h3>
-                            <p class="text-subtext0 line-clamp-2 text-sm">
-                                
-                            </p>
                         </div>
                     </a>
                 </div>
